5757848fa826e17dbfb35e180ba16c9b6bdde651
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-07-08 16:52:58 +0200

    PageTitle

--- src/pages/category-page/Category.js
+++ src/pages/category-page/Category.js
@@ -1,9 +1,17 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
+
+import withRouter from '../../util/hoc/withRouter';
+
+import PageTitle from '../../components/ui/page-title/PageTitle';
 
 class Category extends Component {
   render() {
-    return <div> Category Page </div>;
+    return (
+      <Fragment>
+        <PageTitle>{this.props.router.params.categoryName}</PageTitle>
+      </Fragment>
+    );
   }
 }
 
-export default Category;
+export default withRouter(Category);
