5f0cf7bd73d9330a7f30b07350e4736c15f6d79f
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-07-09 04:51:39 +0200

    currencies

--- src/components/currency/currency-modal/CurrencyModal.js
+++ src/components/currency/currency-modal/CurrencyModal.js
@@ -4,9 +4,35 @@ import Modal from '../../ui/modal/Modal';
 
 import classes from './CurrencyModal.module.css';
 
+const dummpyCurrencies = ['USD', 'EUR', 'JPY']
+
 class CurrencyModal extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currencies: [],
+      selectedCurrency: null,
+    };
+  }
+
+  componentDidMount() {
+    // fetch currenies
+    this.setState({ currencies: dummpyCurrencies});
+  }
+
+  componentDidUpdate() {
+    // update selectedCurrency in the context
+    // if the selectedCurrency state has changed
+  }
+
   render() {
-    return <Modal style={classes.currencyModal}>CurrencyModal</Modal>;
+    return (
+      <Modal style={classes.currencyModal}>
+        <div className={classes.currencyModalContent}>
+          {this.state.currencies.map((currency, i) => <p key={i}> $ {currency} </p>)}
+        </div>
+      </Modal>
+    );
   }
 }
 
