0a4584e38929c8d3160f71cd74a4f2068ba8b83d
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-07-11 02:16:03 +0200

    CategoryModal

---
+++ src/components/products/category-modal/CategoryModal.js
@@ -0,0 +1,65 @@
+import { Component } from 'react';
+
+import SideModal from '../../ui/modal/SideModal';
+
+import classes from './CategoryModal.module.css';
+
+class CategoryModal extends Component {
+  constructor() {
+    super();
+    this.state = { availableCategories: [] };
+  }
+
+  async fetchCategories() {
+    const graphqlQuery = {
+      query: `query {
+          categories {
+            name
+          }
+        }`,
+    };
+
+    try {
+      const response = await fetch('http://localhost:4000', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(graphqlQuery),
+      });
+
+      const resData = await response.json();
+
+      const categories = resData.data.categories.map((c) => c.name);
+
+      this.setState({
+        availableCategories: categories,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  render() {
+    const categoriesModalContent = this.state.availableCategories.map(
+      (categoryName, i) => (
+        <p
+          key={i}
+          onClick={() => this.props.onChangeCatgeory(categoryName)}
+        >
+          {categoryName}
+        </p>
+      )
+    );
+
+    return (
+      <SideModal style={classes.categoryModal}>
+        {categoriesModalContent}
+      </SideModal>
+    );
+  }
+}
+
+export default CategoryModal;
