6b5b715992a18ff91d477a44c11721e96cf8a1b6
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-07-22 21:03:47 +0200

    navigation changes

--- src/components/navbar/navbar-list/NavbarList.js
+++ src/components/navbar/navbar-list/NavbarList.js
@@ -1,19 +1,44 @@
 import { Component } from 'react';
 
+import withHTTP from '../../../util/hoc/withHTTP';
+
 import NavbarItem from './NavbarItem';
 
 import classes from './NavbarList.module.css';
 
 class NavbarList extends Component {
-  render() {
-    return (
-      <ul className={classes.navbarList}>
-        <NavbarItem link='/category/women'>WOMEN</NavbarItem>
-        <NavbarItem link='/category/men'>MEN</NavbarItem>
-        <NavbarItem link='/category/kids'>KIDS</NavbarItem>
-      </ul>
+  constructor() {
+    super();
+    this.state = { availableCategories: [] };
+  }
+
+  async componentDidMount() {
+    this.props.http.fetchData(
+      {
+        query: `query {
+          categories {
+            name
+          }
+        }`,
+      },
+      (data) => {
+        const categories = data.categories.map((c) => c.name);
+        this.setState({
+          availableCategories: categories,
+        });
+      }
     );
+  }
+
+  render() {
+    const navLinks = this.state.availableCategories.map((category) => (
+      <NavbarItem key={category} link={`/category/${category}`}>
+        {category.toUpperCase()}
+      </NavbarItem>
+    ));
+
+    return <ul className={classes.navbarList}>{navLinks}</ul>;
   }
 }
 
-export default NavbarList;
+export default withHTTP(NavbarList);
--- src/pages/category-page/Category.js
+++ src/pages/category-page/Category.js
@@ -1,49 +1,20 @@
 import { Component, Fragment } from 'react';
 
+import withRouter from '../../util/hoc/withRouter';
+
 import PageTitle from '../../components/ui/page-title/PageTitle';
 import ProductList from '../../components/products/product-list/ProductList';
-import CategoryModal from '../../components/products/category-modal/CategoryModal';
 
 class Category extends Component {
-  constructor() {
-    super();
-    this.state = {
-      selectedCategory: 'all',
-      showCategoryModal: false,
-    };
-  }
-
-  onToggleModalHandler() {
-    this.setState((prevState) => {
-      return { showCategoryModal: !prevState.showCategoryModal };
-    });
-  }
-
-  onChangeSelectedCatgeoryHandler(categoryName) {
-    this.setState({
-      selectedCategory: categoryName,
-      showCategoryModal: false,
-    });
-  }
-
   render() {
+    const categoryType = this.props.router.params.categoryType;
     return (
       <Fragment>
-        <PageTitle
-          title={this.state.selectedCategory}
-          onClick={this.onToggleModalHandler.bind(this)}
-          isModalOpen={this.state.showCategoryModal}
-        />
-        {this.state.showCategoryModal && (
-          <CategoryModal
-            onChangeCatgeory={this.onChangeSelectedCatgeoryHandler.bind(this)}
-            onClose={this.onToggleModalHandler.bind(this)}
-          />
-        )}
-        <ProductList categoryName={this.state.selectedCategory} />
+        <PageTitle title={categoryType} />
+        <ProductList categoryName={categoryType} />
       </Fragment>
     );
   }
 }
 
-export default Category;
+export default withRouter(Category);
